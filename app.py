a12fe776ea92768db3484d7785c065390e3645ad
Andrey_Blight <kizhinov05@gmail.com>
2022-12-07 18:39:47 +0300

    Add new functions

--- app.py
+++ app.py
@@ -7,25 +7,59 @@ import requests
 
 load_dotenv()
 bot = telebot.TeleBot(os.environ.get("TELEGRAM_BOT_TOKEN"))
-QUESTIONS = [{"question": "Ответь 1", "ans": "1", "score": 10},
-             {"question": "Ответь 2", "ans": "2", "score": 5},
-             {"question": "Ответь 3", "ans": "3", "score": 6},
-             {"question": "Ответь 4", "ans": "4", "score": 7},
-             {"question": "Ответь 5", "ans": "5", "score": 2}]
-user_questions = dict()
+PASSWORD = os.environ.get("PASSWORD")
+API_PREFIX = os.environ.get("API_PREFIX")
+QUESTIONS = [
+    {"question": "Назовите фильм, массовая сцена из которого занесена в книгу рекордов Гинесса",
+     "ans": "«Война и мир»",
+     "variants": ["«Белое солнце пустыни»", "«Солярис»", "«Война и мир»",
+                  "Киноэпопея «Освобождение»"],
+     "score": 1}]
+user_data = dict()
 
 
 def get_question(user_id: int):
-    index = user_questions[user_id]["indexes"][0]
-    return QUESTIONS[index]["question"]
+    user_index: list = user_data[user_id]["indexes"]
+    if not user_index:
+        user_data.pop(user_id)
+        requests.put(f"{API_PREFIX}/stop", params={"tg_id": user_id})
+        return "Вопросы закончились!!!"
+    else:
+        return QUESTIONS[user_index[0]]["question"]
+
+
+def ans_question(user_id: int, ans) -> str:
+    user_index: list = user_data[user_id]["indexes"]
+    question_now = QUESTIONS[user_index[0]]
+    user_index.pop(0)
+    if ans == question_now["ans"]:
+        requests.put(f"{API_PREFIX}/score",
+                     params={"delta": question_now["score"], "tg_id": user_id})
+        return f"Правильный ответ, вы заработали {question_now['score']} баллов."
+    else:
+        return "Неправильный ответ :("
+
+
+def create_name(user_id: int, name: str):
+    requests.post(f"{API_PREFIX}/add_user", params={"tg_id": user_id, "name": name}).json()
+    user_data[user_id]["status"] = "password"
+    return "Имя успешно сохранено"
+
+
+def get_password(user_id: int, password: str):
+    if PASSWORD == password:
+        user_data[user_id]["status"] = "question"
+        return "Время начать викторину!"
+    return "Неверный код!!!"
 
 
 def init_game(done: bool, score: int | None, user_id: int):
     if score is None:
+        # Начинаем игру если еще нет рекорда
         indexes = [el for el in range(len(QUESTIONS))]
         random.shuffle(indexes)
-        user_questions[user_id] = {"indexes": indexes}
-        return get_question(user_id)
+        user_data[user_id] = {"indexes": indexes, "status": "get_name"}
+        return "Введите свое имя"
     if isinstance(score, int):
         return_string = f"Вы уже {'играли и ' if done else ''}набрали {score} "
         if 10 <= score <= 20:
@@ -41,38 +75,28 @@ def init_game(done: bool, score: int | None, user_id: int):
 
 @bot.message_handler(commands=["start"])
 def start_message(message: telebot.types.Message):
-    user_data = message.from_user
-    first_name = user_data.first_name if user_data.first_name is not None else ''
-    last_name = user_data.last_name if user_data.last_name is not None else ''
-    user_name = f"{first_name} {last_name}"
-    user_id = user_data.id
-    score, done = requests.get("http://127.0.0.1:8000/init_score",
-                               params={"tg_id": user_id, "name": user_name}).json()
+    user_id = message.from_user.id
+    score, done = requests.get(f"{API_PREFIX}/init_score",
+                               params={"tg_id": user_id}).json()
     bot.send_message(message.chat.id, init_game(done, score, user_id))
 
 
 @bot.message_handler()
 def start_message(message: telebot.types.Message):
-    if message.from_user.id not in user_questions:
+    user_id = message.from_user.id
+    if message.from_user.id not in user_data:
         bot.send_message(message.chat.id, "Напишите команду /start чтобы начать.")
         return
     ans = message.text.lower().strip()
-    user_index: list = user_questions[message.from_user.id]["indexes"]
-    question_now = QUESTIONS[user_index[0]]
-    if ans == question_now["ans"]:
-        bot.send_message(message.chat.id,
-                         f"Правильный ответ, вы заработали {question_now['score']} баллов.")
-        requests.put("http://127.0.0.1:8000/score",
-                     params={"delta": question_now["score"], "tg_id": message.from_user.id})
+    if user_data[user_id]["status"] == "get_name":
+        bot.send_message(message.chat.id, create_name(user_id, ans))
+    elif user_data[user_id]["status"] == "password":
+        bot.send_message(message.chat.id, get_password(user_id, ans))
+        if user_data[user_id]["status"] == "question":
+            bot.send_message(message.chat.id, get_question(user_id))
     else:
-        bot.send_message(message.chat.id, "Неправильный ответ :(")
-    user_index.pop(0)
-    if not user_index:
-        user_questions.pop(message.from_user.id)
-        bot.send_message(message.chat.id, "Вопросы закончились!!!")
-        requests.put("http://127.0.0.1:8000/stop", params={"tg_id": message.from_user.id})
-    else:
-        bot.send_message(message.chat.id, get_question(message.from_user.id))
+        bot.send_message(message.chat.id, ans_question(user_id, ans))
+        bot.send_message(message.chat.id, get_question(user_id))
 
 
 if __name__ == '__main__':
